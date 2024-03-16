6dd717e24b2913214e3537fe909ddba0e478ece5
Daniflav94 <danyelly_mell@hotmail.com>
2024-03-16 13:09:16 -0300

    finalização página sobre nós

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,25 +3,28 @@ import paw from "../../assets/images/pata.jpeg";
 
 export function About() {
   return (
-    <S.Container>
-      <S.Image src={paw} alt="" />
-      <S.Content>
-        <h2>Sobre nós</h2>
-        <span>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, quibusdam
-          vero omnis odio cumque tenetur. Quibusdam facilis fugit reprehenderit
-          minus ducimus quisquam. Consectetur vitae odio vero ipsam at labore,
-          illum ducimus eligendi enim quisquam, accusantium quidem soluta
-          ratione, vel magni omnis voluptatibus quos doloribus cupiditate saepe
-          corporis? Fuga sunt, maxime ipsam dignissimos alias at ducimus
-          voluptas, illo laudantium harum eos? Natus consectetur dignissimos
-          iste, veniam mollitia quidem sunt tenetur alias quod repudiandae
-          dolores dolore repellendus accusantium voluptatibus nostrum fugit a
-          ullam nesciunt, deserunt aliquid pariatur. Magnam itaque, a velit,
-          quod aliquam veritatis atque cum sunt error provident inventore,
-          excepturi nemo?
-        </span>
-      </S.Content>
-    </S.Container>
+    <>
+      <S.Background></S.Background>
+      <S.Container>
+        <S.Image src={paw} alt="" />
+        <S.Content>
+          <S.Title>Sobre nós</S.Title>
+          <span>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. A,
+            quibusdam vero omnis odio cumque tenetur. Quibusdam facilis fugit
+            reprehenderit minus ducimus quisquam. Consectetur vitae odio vero
+            ipsam at labore, illum ducimus eligendi enim quisquam, accusantium
+            quidem soluta ratione, vel magni omnis voluptatibus quos doloribus
+            cupiditate saepe corporis? Fuga sunt, maxime ipsam dignissimos alias
+            at ducimus voluptas, illo laudantium harum eos? Natus consectetur
+            dignissimos iste, veniam mollitia quidem sunt tenetur alias quod
+            repudiandae dolores dolore repellendus accusantium voluptatibus
+            nostrum fugit a ullam nesciunt, deserunt aliquid pariatur. Magnam
+            itaque, a velit, quod aliquam veritatis atque cum sunt error
+            provident inventore, excepturi nemo?
+          </span>
+        </S.Content>
+      </S.Container>
+    </>
   );
 }
--- src/pages/about/styles.ts
+++ src/pages/about/styles.ts
@@ -1,19 +1,33 @@
 import styled from "styled-components";
 
+export const Background = styled.div`
+  background-color: ${(props) => props.theme.colors.blue};
+  position: absolute;
+  width: 100vw;
+  height: 40vh;
+  z-index: 2;
+`;
+
 export const Container = styled.section`
-    display: flex;
-    width: 100vw;
-    height: 100vh;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Content = styled.div`
-    width: 40rem;
-    margin-left: 5rem;
+  width: 40rem;
+  margin-left: 5rem;
+  z-index: 999999 !important;
 `;
 
 export const Image = styled.img`
-    width: 15rem;
-    height: 25rem;
+  width: 15rem;
+  height: 25rem;
+  z-index: 99999;
+`;
+
+export const Title = styled.h2`
+    color: ${props => props.theme.colors.nude};
 `;
